ec19c1659005638471195bab56c42ca0126fb7e7
NoniAprillia <165237591+NoniAprillia@users.noreply.github.com>
2024-03-29 10:17:13 +0700

    java script

---
+++ hasil.js
@@ -0,0 +1,13 @@
+function hitung(a, b) {
+  return a * b;
+}
+let a = 5;
+let b = 6;
+let hasil = hitung(aa, bb);
+document.write("Nilai a = " + a + "<br>");
+document.write("Nilai b = " + b + "<br>");
+
+// alert(greet);
+function button() {
+  alert("Bilangan a x b = " + hasil);
+}
